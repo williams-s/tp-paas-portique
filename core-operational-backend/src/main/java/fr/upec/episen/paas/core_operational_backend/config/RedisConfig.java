1eeb8143acb67c076a78100d92ea6ac9a5181835
Wang John <john-wang@hotmail.fr>
2025-10-30 23:41:21 +0100

    Edited redis object model

--- core-operational-backend/src/main/java/fr/upec/episen/paas/core_operational_backend/config/RedisConfig.java
+++ core-operational-backend/src/main/java/fr/upec/episen/paas/core_operational_backend/config/RedisConfig.java
@@ -2,28 +2,22 @@ package fr.upec.episen.paas.core_operational_backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
     @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
-    }
-
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory connectionFactory) {
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
-
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
 
-        template.afterPropertiesSet();
         return template;
     }
 }
+
--- core-operational-backend/src/main/java/fr/upec/episen/paas/core_operational_backend/service/StudentService.java
+++ core-operational-backend/src/main/java/fr/upec/episen/paas/core_operational_backend/service/StudentService.java
@@ -3,8 +3,12 @@ package fr.upec.episen.paas.core_operational_backend.service;
 import java.time.LocalTime;
 import java.time.ZoneId;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.RequiredArgsConstructor;
 import paas.tp.common.backend.models.Student;
@@ -14,17 +18,22 @@ import paas.tp.common.backend.models.Student;
 public class StudentService {
     
     private final RedisTemplate<String, Object> redisTemplate;
+    private final static Logger logger = LogManager.getLogger(StudentService.class);
 
     public Student getStudentIfAllowed(Long id) {
         LocalTime time = LocalTime.now(ZoneId.of("Europe/Paris"));
         if (time.isBefore(LocalTime.of(8,0)) || time.isAfter(LocalTime.of(21,0))) {
-            //return null;
+            return null;
         }
 
         String key = "student:" + id;
         Object studentObj = redisTemplate.opsForValue().get(key);
-        if (studentObj instanceof Student && ((Student)studentObj).isShouldOpen()) {
-            return (Student) studentObj;
+        ObjectMapper mapper = new ObjectMapper();
+        Student student = mapper.convertValue(studentObj, Student.class);
+
+        if (student.isShouldOpen()) {
+            logger.info("Student {} is allowed to open the door.", student.toString());
+            return student;
         }
         return null;
     }
