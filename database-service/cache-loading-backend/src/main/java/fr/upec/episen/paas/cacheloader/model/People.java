d3a92c9e57f3cf8a5ab0009a0f5c35a3138d4e27
Clement Taurand <clment2402@gmail.com>
2025-11-10 22:45:16 +0100

    Application du changement sur le model

--- database-service/cache-loading-backend/src/main/java/fr/upec/episen/paas/cacheloader/model/People.java
+++ database-service/cache-loading-backend/src/main/java/fr/upec/episen/paas/cacheloader/model/People.java
@@ -5,7 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
-import java.time.OffsetDateTime; // Utilisation de OffsetDateTime pour TIMESTAMP WITH TIME ZONE
+import java.time.LocalDateTime;
+ // Utilisation de OffsetDateTime pour TIMESTAMP WITH TIME ZONE
 
 @Entity // Indique que cette classe est une entité JPA
 @Table(name = "people") // Mappe à la table 'registered_people' dans la DB
@@ -31,11 +32,11 @@ public class People {
     private Profile function;
 
     @Column(name = "allowed_interval_start")
-    private OffsetDateTime allowedIntervalStart;
+    private LocalDateTime allowedIntervalStart;
 
     @Column(name = "allowed_interval_end")
-    private OffsetDateTime allowedIntervalEnd;
+    private LocalDateTime allowedIntervalEnd;
 
     @Column(name = "registration_date")
-    private OffsetDateTime registrationDate;
+    private LocalDateTime registrationDate;
 }
